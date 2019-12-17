45953bb87c234c5282024de0001a918d8b25c9fa
weng lin <wenglin@buffalo.edu>
2019-12-17 06:55:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0
 
   while counter < array.length do
-    new_numbers << ([array[counter]**2])
+    new_numbers.push([array[counter]**2])
     counter += 1
   end
   return new_numbers
